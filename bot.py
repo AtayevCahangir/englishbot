1718d198284a3571e8735be5f4ef132746069a95
Aylin1101 <aylindadashli777@gmail.com>
2025-07-11 10:20:23 +0400

    create project

---
+++ bot.py
@@ -0,0 +1,12 @@
+import asyncio 
+from create_bot import application
+from handlers import  client
+async def main()->None:
+    await application.initialize()
+    await application.start()
+    await application.udapter.start_polling()
+    await application.run_polling()
+if __name__ =="__main__":
+    application.run_polling()
+
+       
---
+++ create_bot.py
@@ -0,0 +1,6 @@
+import os 
+from telegram.ext import ApplicationBuilder
+
+TOKEN="8088013739:AAEedIQEy-mju-0qjaK3lahmoj2MnUd6rFs"
+
+application = ApplicationBuilder().token(TOKEN).build()
